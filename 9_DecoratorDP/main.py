3fb475d5e70f8fc4c89d390cf4b5c40d39386522
Backbencher05 <adiksoni095@gmail.com>
2026-06-16 13:05:53 +0530

    Decorator

--- 9_DecoratorDP/main.py
+++ 9_DecoratorDP/main.py
@@ -4,5 +4,6 @@ from cheezeAddons import Cheese
 if __name__ == "__main__":
     pza = BasePizza()
     print(pza.get_price()) # we don't wan't any addons
-    cheeze_pza = Cheese(pza)
+    # let add cheese on the top of our pizza 
+    cheeze_pza = Cheese(pza) 
     print(cheeze_pza.get_price())
